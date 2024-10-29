714201e2175664b4923d9b77cde6a48329627fd6
B211088 <139199932+B211088@users.noreply.github.com>
2024-10-30 04:11:55 +0700

    Update accounts.jsx

--- client/src/redux/reducers/accounts.jsx
+++ client/src/redux/reducers/accounts.jsx
@@ -27,11 +27,27 @@ export default function accountsReducers(state = INIT_STATE, action) {
         isLoading: false,
         error: action.error, // Lưu trữ lỗi từ action
       };
+    case getType(createAccount.createAccountRequest):
+      return {
+        ...state,
+        isLoading: true, // Bắt đầu yêu cầu tạo tài khoản
+      };
     case getType(createAccount.createAccountSuccess):
       return {
         ...state,
         data: [...state.data, action.payload],
         isLoading: false, // Đặt thành false khi thành công
+      };
+    case getType(createAccount.createAccountFailure):
+      return {
+        ...state,
+        isLoading: false,
+        error: action.error, // Lưu trữ lỗi khi tạo tài khoản thất bại
+      };
+    case getType(updateAccount.updateAccountRequest):
+      return {
+        ...state,
+        isLoading: true, // Bắt đầu yêu cầu cập nhật tài khoản
       };
     case getType(updateAccount.updateAccountSuccess):
       return {
@@ -39,26 +55,32 @@ export default function accountsReducers(state = INIT_STATE, action) {
         data: state.data.map((account) =>
           account._id === action.payload._id ? action.payload : account
         ),
+        isLoading: false, // Đặt thành false khi cập nhật thành công
       };
     case getType(updateAccount.updateAccountFailure):
       return {
         ...state,
         isLoading: false,
-        error: action.message, // Lưu trữ thông điệp lỗi
+        error: action.error, // Lưu trữ thông điệp lỗi
       };
-    case getType(deleteAccount.deleteAccountSuccess): // Thêm case cho thành công xóa tài khoản
+    case getType(deleteAccount.deleteAccountRequest):
+      return {
+        ...state,
+        isLoading: true, // Bắt đầu yêu cầu xóa tài khoản
+      };
+    case getType(deleteAccount.deleteAccountSuccess):
       return {
         ...state,
         data: state.data.filter(
           (account) => account._id !== action.payload._id
         ), // Xóa tài khoản khỏi danh sách
-        isLoading: false,
+        isLoading: false, // Đặt thành false khi xóa thành công
       };
-    case getType(deleteAccount.deleteAccountFailure): // Thêm case cho thất bại xóa tài khoản
+    case getType(deleteAccount.deleteAccountFailure):
       return {
         ...state,
         isLoading: false,
-        error: action.message, // Lưu trữ thông điệp lỗi
+        error: action.error, // Lưu trữ thông điệp lỗi
       };
     default:
       return state;
