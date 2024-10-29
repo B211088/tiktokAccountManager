55a0f9ebb795fa5bcee41c5adbd0251dd96c6764
B211088 <139199932+B211088@users.noreply.github.com>
2024-10-30 04:06:54 +0700

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,34 +2,39 @@ import AddAcount from "../components/AddAccount/AddAcount";
 import Header from "../components/header/Header";
 import ListAcount from "../components/ListAccount/ListAcount";
 import Tools from "../components/Tools/Tools";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { AppProvider } from "../context";
 
 const HomePage = () => {
   const [statusAddAccount, setStatusAddAccount] = useState(false);
-  const [refreshList, setRefreshList] = useState(false); // State to control refresh
+  const [refreshList, setRefreshList] = useState(false);
 
   const changeStatusAddAccount = () => {
     setStatusAddAccount(!statusAddAccount);
   };
 
-  const refreshComponent = () => {
-    setRefreshList((prev) => !prev);
+  const handleAddAccountSubmit = () => {
+    setStatusAddAccount(false);
   };
 
-  useEffect(() => {}, [refreshList]);
+  const handleRefreshList = () => {
+    setRefreshList(!refreshList);
+  };
 
   return (
-    <div className="w-full flex flex-col justify-center items-center relative ">
-      <Header />
-      <Tools onClick={changeStatusAddAccount} />
-      {statusAddAccount && (
-        <AddAcount
-          onClose={changeStatusAddAccount}
-          onSuccess={refreshComponent}
-        />
-      )}
-      <ListAcount key={refreshList} refreshList={refreshComponent} />
-    </div>
+    <AppProvider>
+      <div className="w-full flex flex-col justify-center items-center relative">
+        <Header />
+        <Tools onClick={changeStatusAddAccount} />
+        {statusAddAccount && (
+          <AddAcount
+            onClose={handleAddAccountSubmit}
+            submitSucces={handleRefreshList}
+          />
+        )}
+        <ListAcount refreshList={refreshList} onEdit={handleRefreshList} />
+      </div>
+    </AppProvider>
   );
 };
 
