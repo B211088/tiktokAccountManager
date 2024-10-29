5857f07a95053a126bdadf78b0921dccf501b173
B211088 <139199932+B211088@users.noreply.github.com>
2024-10-30 04:06:11 +0700

    Update Account.jsx

--- client/src/components/ListAccount/Account.jsx
+++ client/src/components/ListAccount/Account.jsx
@@ -2,29 +2,31 @@ import { useState, useCallback, useRef, useEffect } from "react";
 import UpdateAccount from "../UpdateAccount/UpdateAccount";
 import { useDispatch } from "react-redux";
 import * as actions from "../../redux/actions";
+import ButtonAction from "./ButtonAction";
 
 const Account = ({ account, index, onEdit }) => {
   const currentDate = Date.now();
   const timeDifference = currentDate - account.date;
   const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-  const newDataAccount = {
-    ...account,
-    date: daysDifference,
-  };
-
   const dispatch = useDispatch();
-  const [isModify, setIsModify] = useState(false);
   const [isOpenUpdateModal, setIsOpenUpdateModal] = useState(false);
+  const [isActionMenuOpen, setIsActionMenuOpen] = useState(false);
   const modalRef = useRef(null);
 
-  const changeStatusModalUpdate = () => {
+  const [data, setData] = useState(account);
+
+  useEffect(() => {
+    setData(account);
+  }, [account]);
+
+  const handleChangModalUpdate = () => {
     setIsOpenUpdateModal(!isOpenUpdateModal);
   };
 
-  const changeStatusModal = (e) => {
-    e.stopPropagation();
-    setIsModify(!isModify);
+  const handleCancelModalUpdate = (e) => {
+    setIsOpenUpdateModal(!isOpenUpdateModal);
+    e.stopPropagation;
   };
 
   const handleDeleteAccount = useCallback(
@@ -38,14 +40,14 @@ const Account = ({ account, index, onEdit }) => {
         onEdit();
       }
     },
-    [dispatch, account, onEdit]
+    [dispatch, account]
   );
 
-  const handleUpdateSuccess = () => {
-    setIsOpenUpdateModal(false);
-    onEdit();
+  const toggleActionMenu = (e) => {
+    e.stopPropagation();
+    setIsActionMenuOpen(!isActionMenuOpen);
   };
-    const handleResetTime = useCallback(() => {
+  const handleResetTime = useCallback(() => {
     const newDataAccount = {
       ...account,
       date: currentDate,
@@ -57,7 +59,7 @@ const Account = ({ account, index, onEdit }) => {
   const handleClickOutside = (e) => {
     if (modalRef.current && !modalRef.current.contains(e.target)) {
       setIsOpenUpdateModal(false);
-      setIsModify(false);
+      setIsActionMenuOpen(false);
     }
   };
 
@@ -70,27 +72,27 @@ const Account = ({ account, index, onEdit }) => {
 
   return (
     <div
-      className="w-full flex flex-wrap items-center justify-between bg-bg-light border-[1px] rounded-[5px] border-cl-border px-[10px] gap-[10px] cursor-pointer relative"
+      className="w-full h-full max-h-[150px] sm:h-[68px] flex flex-wrap items-center justify-between  bg-bg-light border-[1px] rounded-[5px] border-cl-border px-[10px] gap-[10px] cursor-pointer relative "
       onClick={() => {
-        setIsModify(false);
         setIsOpenUpdateModal(false);
+        setIsActionMenuOpen(false);
       }}
     >
       <div className="w-[30px] min-w-[30px] rounded-[5px] border-cl-border border-[1px] text-center font-bold leading-[26px]">
         {index + 1}
       </div>
       <div className="flex min-w-[230px] sm:max-w-[80%] sm:flex-row flex-col flex-1  justify-center items-center flex-wrap text-left">
-        <div className="flex-1 sm:w-[20%] w-full min-w-[150px] sm:h-[68px] sm:leading-[68px] h-[20px]   font-bold px-[5px] leading-[30px] text-[0.8rem] overflow-hidden whitespace-nowrap text-ellipsis">
+        <div className="flex-1 sm:w-[20%] w-full min-w-[150px] sm:h-[68px] sm:leading-[68px] h-[20px] font-bold px-[5px] leading-[30px] text-[0.8rem] overflow-hidden whitespace-nowrap text-ellipsis">
           IDTIKTOK: {account.idTiktok}
         </div>
         <div className="flex-1 sm:w-[50%] w-full min-w-[40%] sm:h-[68px] sm:leading-[68px] h-[60px] font-bold px-[5px] leading-[30px] text-[0.8rem] sm:overflow-hidden sm:whitespace-nowrap text-ellipsis break-words">
           Google Account: {account.accountGoogle}
         </div>
 
-        <div className="flex-1 sm:w-[20%] w-full min-w-[60px] sm:h-[68px] sm:leading-[68px] h-[30px]  font-bold px-[5px] leading-[30px] text-[0.8rem] overflow-hidden whitespace-nowrap text-ellipsis">
+        <div className="flex-1 sm:w-[20%] w-full min-w-[60px] sm:h-[68px] sm:leading-[68px] h-[30px] font-bold px-[5px] leading-[30px] text-[0.8rem] overflow-hidden whitespace-nowrap text-ellipsis">
           Ngày: {daysDifference}
         </div>
-        <div className="flex-1 sm:w-[20%] w-full min-w-[160px] sm:h-[68px] sm:leading-[68px] h-[30px]   font-bold px-[5px] leading-[30px] text-[0.8rem]">
+        <div className="flex-1 sm:w-[20%] w-full min-w-[160px] sm:h-[68px] sm:leading-[68px] h-[30px] font-bold px-[5px] leading-[30px] text-[0.8rem]">
           Máy:{" "}
           {account.pertain == null || account.pertain === 0
             ? "Chưa đăng nhập"
@@ -104,51 +106,26 @@ const Account = ({ account, index, onEdit }) => {
           }`}
         ></div>
       </div>
-      <div className="min-w-[30px] rounded-[5px] border-cl-border border-[1px] text-center font-bold leading-[26px] text-[1.2rem]">
-        <div className="w-[30px] h-[30px] leading-[30px] learelative" onClick={changeStatusModal}>
+      <div className="min-w-[30px] rounded-[5px] border-cl-border border-[1px] text-center font-bold leading-[26px] text-[1.2rem] relative">
+        <div
+          className="w-[30px] h-[30px] leading-[30px] relative"
+          onClick={toggleActionMenu}
+        >
           <i className="fa-solid fa-ellipsis-vertical"></i>
-          {isModify && (
-            <div
-              ref={modalRef}
-              className="absolute w-[160px] top-[33px] right-0 z-2 text-[0.8rem] border-[1px] border-cl-border bg-bg-light rounded-[5px] z-[4]"
-            >
-              <ul>
-                <li
-                  className="relative h-[30px] hover:bg-bg-btn-hover bg-bg-light"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    changeStatusModalUpdate();
-                  }}
-                >
-                  Chỉnh sửa
-                </li>
-                {isOpenUpdateModal && (
-                  <div onClick={(e) => e.stopPropagation()}>
-                    <UpdateAccount
-                      account={account}
-                      onClose={changeStatusModalUpdate}
-                      onSuccess={handleUpdateSuccess}
-                    />
-                  </div>
-                )}
-                <li
-                  className="h-[30px] hover:bg-bg-btn-hover bg-bg-light"
-                  onClick={handleDeleteAccount}
-                >
-                  Xóa
-                </li>
-                <li
-                  className="relative h-[30px] hover:bg-bg-btn-hover bg-bg-light"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setIsModify(false);
-                    handleResetTime();
-                  }}
-                  >
-                  Đặt lại thời gian
-                </li>
-              </ul>
-            </div>
+          {isActionMenuOpen && (
+            <ButtonAction
+              onModify={handleChangModalUpdate}
+              onDelete={handleDeleteAccount}
+              onResetTime={handleResetTime}
+            />
+          )}
+          {isOpenUpdateModal && (
+            <UpdateAccount
+              account={account}
+              onClose={handleChangModalUpdate}
+              onCancel={handleCancelModalUpdate}
+              submitSucces={onEdit}
+            />
           )}
         </div>
       </div>
