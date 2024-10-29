396c72837d90d40af563639fd65be1c5b085470e
B211088 <139199932+B211088@users.noreply.github.com>
2024-10-30 04:27:13 +0700

    Update index.jsx

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://tiktokaccountmanager.onrender.com";
+const URL = import.meta.env.VITE_API_URL;
 
 export const fetchAccounts = () => axios.get(`${URL}/accounts`);
 export const createAccount = (payload) =>
